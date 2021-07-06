2fca2213d1e02476f7ae558c51e2f36f820862d5
aantoshkin <antoshkin@glolime.com>
2021-07-06 21:28:25 +0300

    + job chain

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/job1', function () {
+    App\Jobs\SendMessage::dispatch("Test message");
+});
+
+/**
+ *  добавляет в таблицу jobs запись
+ *  делает отсрочку
+ *  выполняет джобу (удаляет из таблицы jobs)
+ */
+Route::get('/job2', function () {
+    $job = new App\Jobs\SendMessage("job2");
+    $job->delay(60);
+    dispatch($job)->onQueue('myQueue');
+});
+
+
+Route::get('/job_chain', function () {
+    App\Jobs\JobTest1::withChain([
+        new App\Jobs\JobTest2("step1"),
+        new App\Jobs\JobTest3("step2"),
+    ])->dispatch("start job");
+});
+
+
